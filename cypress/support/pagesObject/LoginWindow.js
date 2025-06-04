67c0735e7143f58a3d6fc5515ba33864a2048b56
Ana <araperaltamoreno@gmail.com>
2025-06-04 15:26:21 -0500

    feat(login): add typeUsername and typePassword methods

--- cypress/support/pagesObject/LoginWindow.js
+++ cypress/support/pagesObject/LoginWindow.js
@@ -1,11 +1,18 @@
-import HomePage from './HomePage'; 
+import HomePage from './HomePage';
 
 const locators = Cypress.env("locators");
 
-class LoginWindow extends HomePage { 
+class LoginWindow extends HomePage {
     getUsernameInput = () => cy.get(locators.idLoginUsername);
     getPasswordInput = () => cy.get(locators.idLoginPassword);
     getLoginSubmitButton = () => cy.get(locators.idLoginSubmit);
+
+    typeUsername(username) {
+        this.getUsernameInput().type(username);
+    }
+    typePassword(password) {
+        this.getPasswordInput().type(password);
+    }
 }
 
 export default LoginWindow;
