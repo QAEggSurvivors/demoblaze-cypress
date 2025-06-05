18448e25a45ba73e7709c4e5871470b8f0887609
emzimaeduca <emzimaeduca@gmail.com>
2025-06-05 15:26:15 -0300

    fix(support): Updated index.js for the new page object and the method for handling js errors (does not affect testing)

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -11,21 +11,32 @@ import PlaceOrderWindow from "./pagesObject/PlaceOrderWindow";
 import ProductPage from "./pagesObject/ProductPage";
 import PurchaseWindow from "./pagesObject/PurchaseWindow";
 import SignupWindow from "./pagesObject/SignupWindow";
+import CommandsUtilsPage from "./pagesObject/CommandsUtilsPage";
 
 beforeEach(() => {
-  cy.CartPage = new CartPage();
-  cy.ContactWindow = new ContactWindow();
+  // cy.CartPage = new CartPage();
+  // cy.ContactWindow = new ContactWindow();
   cy.HomePage = new HomePage();
-  cy.LoginWindow = new LoginWindow();
-  cy.PlaceOrderWindow = new PlaceOrderWindow();
-  cy.ProductPage = new ProductPage();
-  cy.PurchaseWindow = new PurchaseWindow();
-  cy.SignupWindow = new SignupWindow();
+  // cy.LoginWindow = new LoginWindow();
+  // cy.PlaceOrderWindow = new PlaceOrderWindow();
+  // cy.ProductPage = new ProductPage();
+  // cy.PurchaseWindow = new PurchaseWindow();
+  // cy.SignupWindow = new SignupWindow();
+  cy.CommandsUtilsPage = new CommandsUtilsPage();
   cy.fixture("locators").then((data) => Cypress.env("locators", data));
+  // cy.fixture("laptops").then(data => Cypress.env("laptops", data));
+  // cy.fixture("monitors").then(data => Cypress.env("monitors", data));
+  // cy.fixture("phones").then(data => Cypress.env("phones", data));
 });
 
 Cypress.on("uncaught:exception", (err, runnable) => {
   if (err.message.includes("Bootstrap's JavaScript requires jQuery")) {
+    return false;
+  }
+});
+
+Cypress.on("uncaught:exception", (err) => {
+  if (err.message.includes("hls.demoblaze.com")) {
     return false;
   }
 });
