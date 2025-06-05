4259ec55c1cff6082515a0a8226591cbf3d307f4
emzimaeduca <emzimaeduca@gmail.com>
2025-06-05 15:21:42 -0300

    fix(plugins): Updated index.js to remove files

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -3,10 +3,21 @@
 /**
  * @type {Cypress.PluginConfig}
  */
+const fs = require('fs');
+const path = require('path');
 const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 const cucumber = require("cypress-cucumber-preprocessor").default;
 
 module.exports = (on, config) => {
+  on('task', {
+    deleteFileIfExists(filePath) {
+      if (fs.existsSync(filePath)) {
+        fs.unlinkSync(filePath);
+        return `Archivo ${filePath} eliminado`;
+      }
+      return `Archivo ${filePath} no existía`;
+    }
+  });
   allureWriter(on, config);
   on("file:preprocessor", cucumber());
   return config;
