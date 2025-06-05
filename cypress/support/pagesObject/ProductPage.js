c50fd89ca75b3818cf2f93fb6eb08bd1234889b6
SoyEdwinCabrera <139647637+SoyEdwinCabrera@users.noreply.github.com>
2025-06-04 19:53:15 -0500

    Necessary methods and functions in other files are removed, and typos are corrected

--- cypress/support/pagesObject/ProductPage.js
+++ cypress/support/pagesObject/ProductPage.js
@@ -6,7 +6,7 @@ class ProductPage extends HomePage {
 
   getProductTitle = () => cy.get(locators.classProductTitle);
   getProductPrice = () => cy.get(locators.classProductPrice);
-  getPorductDescription = () => cy.get(locators.idProductDescription);
+  getProductDescription = () => cy.get(locators.idProductDescription);
   getProductImage = () => cy.get(locators.classProductImage);
   getAddToCartButton = () => cy.get(locators.classAddToCartButton);
   
@@ -20,27 +20,7 @@ class ProductPage extends HomePage {
     return this.getProductPrice().invoke("text");
   }
   getProductDescriptionText() {
-    return this.getPorductDescription().invoke("text");
-  }
-  
-  verifyProducttitle(expectedTitle) { 
-    this.getProductTitle().should("contain.text", expectedTitle);
-  }
-  verifyProductPrice(expectedPrice) { 
-    this.getProductPrice().should("contain.text", expectedPrice);
-  }
-  verifyProductImageIsVisible(){
-    this.getProductImage().should("be.visible");
-  }
-  verifyAddToCartButtonIsVisible() {
-    this.getAddToCartButton().should("be.visible").and("contain.text", "Add to cart");
-  }
-  
-  handleAddToCartAlert() {
-    cy.on("window:alert", (str) => {
-      expect(str).to.equal("Product added.");
-    }).as("alert");
-    cy.wait("@alert"); 
+    return this.getProductDescription().invoke("text");
   }
 }
 
