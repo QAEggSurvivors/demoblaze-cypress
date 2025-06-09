2cb31c0656728052e98702e238fe418366cb6540
SoyEdwinCabrera <139647637+SoyEdwinCabrera@users.noreply.github.com>
2025-06-09 18:41:58 -0500

    fix(cardsPage.js) The locators are updated and the path to access them changes

--- cypress/support/pagesObject/cardsPage.js
+++ cypress/support/pagesObject/cardsPage.js
@@ -1,6 +1,14 @@
-import HomePage from "./HomePage.js";
+import HomePage from "./HomePage";
+
+const LOCATORS = require('../locators');
 
 class CardsPage extends HomePage {
+  getLogo = () => cy.get(locators.idLinkLogo);
+  getCardImages = () => cy.get(locators.cssCardImages);
+  getCardTitles = () => cy.get(locators.cssCardTitles);
+  getCardPrices = () => cy.get(locators.cssCardPrices);
+  getCardDescriptions = () => cy.get(locators.cssCardDescriptions);
+
   clickCardImage(number) {
     this.clickProductImage(number);
   }
