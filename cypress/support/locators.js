f382eca67d28e8919bd59ba701aa46dbd6b8aa3c
MiltonKlun <miltonericklun@gmail.com>
2025-06-11 04:37:26 -0300

    feat(locators): Add a locator for place order button on cart

--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -71,5 +71,6 @@ module.exports = {
   idLoginUsername: "#loginusername",
   idLoginPassword: "#loginpassword",
   cssLoginSubmit: "#logInModal .btn.btn-primary",
+  cssPlaceOrderButtonOnCart: ".btn-success[data-toggle='modal']",
   _EndOfFile: "----------------------------"
 }
