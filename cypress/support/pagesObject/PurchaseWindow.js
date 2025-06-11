ba5dd2cc903e8f5eba2bbb72d2c1c068638f6e37
MiltonKlun <miltonericklun@gmail.com>
2025-06-11 04:39:23 -0300

    feat(PurchaseWindow): Create the purchase window flow file

--- cypress/support/pagesObject/PurchaseWindow.js
+++ cypress/support/pagesObject/PurchaseWindow.js
@@ -1,29 +1,29 @@
-// // BORRAR ESTE TEXTO ES SOLO CON CARACTER INFORMATIVO
-// // UNA REFERENCIA ESTA EN LA PAGINA ExamplePage.js
-// // se importa la pagina de la que se heredan todas atributos y metodos
-// // luego se extiende esa clase para que la herencia suceda:
-// import ProductPage from './ProductPage';
-// class ExamplePage extends ProductPage {
-// }
-// export default ExamplePage;
+import HomePage from './HomePage'; 
 
-// // los metodos se identifican la acción que hacen:
-// // si solo quiero traer un locator para luego usarlo
-// // se debe siempre traer el locator para luego usarlo a menos que tenga
-// // alguna condicion especial que haga que mejor que no
-// getLocator() = {return cy.get(pageLocatorCypress.env('pageLocators').NombreLocator).type(username)}
+const locators = Cypress.env("../locators");
 
-// // por ejemplo hacer clic en el button login 
-// clickLoginButton() {return getLocator().click()}
+class PurchaseWindow extends HomePage { 
+  getProductByName = (productName) => cy.get(locators.productTitlesClass).contains(productName);
+  getAddToCartButton = () => cy.get(locators.classAddToCartButton);
+  getCartLink = () => cy.get(locators.linkCartId);
+  getPlaceOrderButtonOnCart = () => cy.get(locators.cssPlaceOrderButtonOnCart);
+  getConfirmButton = () => cy.get(locators.classConfirmButton);
 
-// // por ejemplo escribir en el campo username 
-// typeInputUsername(username) {return getLocator().type(username)}
-// // tip se puede colocar un valor por defecto tal que si la funcion se llama vacia
-// // toma ese valor para el caso
-// typeInputUsername(username = "Carlos") {return getLocator().type(username)}
+  selectProductByName(productName) {
+    this.getProductByName(productName).click();
+  }
+  addToCart() {
+    this.getAddToCartButton().click();
+  }
+  goToCart() {
+    this.getCartLink().click();
+  }
+  clickPlaceOrderButtonOnCart() {
+    this.getPlaceOrderButtonOnCart().click();
+  }
+  clickConfirmButton() {
+    this.getConfirmButton().click({ force: true }); 
+  }
+}
 
-// // para usar los fixture pageLocators,expectedData,laptopsData,monitorsData,phonesData
-// // se usa la expresión en cualquier pagina en cualquier ambito
-// Cypress.env('fixture').nameVariable 
-
-// // Se usa estandar comillas dobles siempre primero y posteriormente a lo interno comillas simples según se requiera
+export default PurchaseWindow;
