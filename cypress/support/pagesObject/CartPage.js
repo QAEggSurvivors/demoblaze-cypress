be2882fda1bc464be856913107d3782ea282187e
Richard Almanza <al.vegari@gmail.com>
2025-06-22 21:38:12 -0500

    Revert 27 draft emzima (#28)
    * Revert "draft(reviewDavid): fix(HomePage.js): Include new methods and fix others fix(header.feature): Include new scenarios and adapt others to perform better steps fix(header.js): Steps to handle the scenarios proposed by David"
    
    This reverts commit f1348879afcbb2e88d5465f5ec4224c6a9e3868b.
    
    * Revert "draft(reviewEdwin): fix(CartPage.js): Include new methods and fix others fix(cart.feature): Include new scenarios and adapt others to perform better steps fix(cart.js): Steps to handle the scenarios proposed by Edwin"
    
    This reverts commit 9e8535260e5f53b5c1ce00c8e8ea6848fc2cc2fe.

--- cypress/support/pagesObject/CartPage.js
+++ cypress/support/pagesObject/CartPage.js
@@ -6,65 +6,30 @@ class CartPage extends HomePage {
   getCartRows = () => cy.get(LOCATORS.productRowOnCartCss);
   getCartProductTitle = ($row) => cy.wrap($row).find(LOCATORS.productTitleOnCartCss).then(text => text.trim());
   getCartProductPrice = ($row) => cy.wrap($row).find(LOCATORS.productPriceOnCartCss).then(text => text.trim());
-  getCartTotalPrice() {
-    return cy.get("tbody#tbodyid tr:visible", {
-        timeout: 20000
-      })
-      .should("have.length", Cypress.env("selectedProductName").length)
-      .then(() => {
-        return cy.get(LOCATORS.totalPriceId)
-          .should('not.be.empty')
-          .invoke('text')
-          .then(text => {
-            return Number(text.trim()) || 0;
-          });
-      });
-  }
+  getCartTotalPrice = () => cy.get(LOCATORS.totalPriceId).invoke('text').then(text => text.trim());
   getDeleteButton = ($row) => cy.wrap($row).find(LOCATORS.deleteButtonClass);
-
-  clickPlaceOrderButton = () => {
+  
+  clickPlaceOrderButton = () =>{
     cy.get(LOCATORS.placeOrderButtonCss).click();
   }
-
+  
   verifyProductInCart = (productName) => {
     return this.getCartRows().filter((index, row) => {
       return Cypress.$(row).text().includes(productName)
-    }).should('have.length.gt', 0);
+    }).should('have.lenght.gt', 0);
   }
-
+  
   removeProductFromCart = (productName) => {
     this.getCartRows().filter((index, row) => {
       return Cypress.$(row).text().includes(productName);
     }).first().then(($row) => {
       this.clickDeleteButton($row);
     });
-  }
-
+  };
+  
   verifyCartTotalPrice = (expectedTotal) => {
-    return this.getCartTotalPrice().should('equal', expectedTotal.toString());
-  }
-  getSelectedProductsWithPrices() {
-    const SELECTED_NAMES = Cypress.env("selectedProductName") || [];
-
-    return SELECTED_NAMES.map(name => this.getProductDataFromFixtures(name))
-      .map(product => ({
-        name: product.name,
-        price: Number(String(product.price).replace(/\$/g, ''))
-      }));
-  }
-  getTotalPriceOfSelected() {
-    const PRODUCTS = this.getSelectedProductsWithPrices();
-    return PRODUCTS.reduce((total, product) => {
-      return total + (Number(product.price) || 0);
-    }, 0);
-  }
-  clickDeleteFromCart(titleProduct) {
-    return cy.clickDeleteFromCart("oneName", titleProduct)
-  }
-  getListProductsInCart() {
-    return cy.getListProductsInCart();
-  }
-
+    this.getCartTotalPrice().should('equal', expectedTotal.toString());
+  };
 }
 
 export default CartPage;
